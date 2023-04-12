6db2de31ccc88e0ce555c7a480c24e423edeb6bb
Kalil Aziz Santos Chami <kalilazizsantoschami@hotmail.com>
2023-04-11 21:52:04 -0300

    initial commit

---
+++ src/pages/about.tsx
@@ -0,0 +1,11 @@
+import Link from "next/link";
+
+export default function About() {
+  return (
+    <div>
+      <h1>Hello Word - About</h1>
+      <p>asdvasdvasdva</p>
+      <Link href="/">Home</Link>
+    </div>
+  );
+}
